f3a49034f3acdc787257332ea097cd702672f660
MailonBerni <165777995+MailonBerni@users.noreply.github.com>
2024-04-30 09:03:02 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -7,8 +7,8 @@ overlay.style.cssText = `
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 80%;
+    height: 80%;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
@@ -16,8 +16,8 @@ overlay.style.cssText = `
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
+    max-width: 80%;
+    max-height: 80%;
     display: none;
     position: absolute;
     top: 50%;
